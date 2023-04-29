52206563b1058a35c20c151bbe4ca4a8e1fb5f6b
Abdulrahman <1xvlux1@gmail.com>
2023-04-30 00:03:42 +0300

    Add class Notes with empty methods

---
+++ notes-class.js
@@ -0,0 +1,33 @@
+class Notes{
+    dbName = 'Notes';
+    dbVersion = 1;
+
+    connect(){
+        // connect to indexedDB, create object store(table) as notes
+    }
+
+    accessStore(){
+        // access to notes Object store (table) with select specific mode('readwrite' or 'readonly').
+
+    }
+    add(){
+        // add new note
+    }
+
+    update(){
+        // update exist note
+    }
+
+    all(){
+        // show all notes on webpage
+    }
+
+    delete(){
+        //delete exist note
+    }
+
+    clear(){
+        // clear all notes on webpage
+    }
+
+}

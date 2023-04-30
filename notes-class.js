01585d82334bfcd0e8e58d5c554641b97d225561
Abdulrahman <1xvlux1@gmail.com>
2023-04-30 23:52:00 +0300

    Add the implementation methods

--- notes-class.js
+++ notes-class.js
@@ -1,33 +1,68 @@
 class Notes{
     dbName = 'Notes';
     dbVersion = 1;
+    reverse = false; //Reverse order of display notes
 
     connect(){
         // connect to indexedDB, create object store(table) as notes
-    }
+        return new Promise((resolve, reject)=> {
+            const request = indexedDB.open(this.dbName, this.dbVersion);
 
-    accessStore(){
+            request.onupgradeneeded = ()=>{
+                let db = request.result;
+                if(!db.objectStoreNames.contains('notes')){
+                    db.createObjectStore('notes', {keyPath: 'id', autoIncrement:true});
+                }
+            }
+
+            request.onsuccess = ()=> {
+                resolve(request.result);
+
+            }
+
+            request.onerror = () =>{
+                reject(request.error.message);
+            }
+
+        })};
+
+
+    async accessStore(mode){
         // access to notes Object store (table) with select specific mode('readwrite' or 'readonly').
+        let connect = await this.connect();
+        let tx = connect.transaction('notes', mode);
+        return tx.objectStore('notes');
+
 
     }
-    add(){
+    async add(note){
         // add new note
+        let accessDB = await this.accessStore('readwrite');
+        accessDB.add(note);
     }
 
-    update(){
+    async update(note){
         // update exist note
+        let accessDB = await this.accessStore('readwrite');
+        accessDB.put(note);
     }
 
-    all(){
+    async all(){
         // show all notes on webpage
+        let accessDB = await this.accessStore('readonly');
+        return accessDB.openCursor(null, this.reverse ? 'prev' : 'next' ); // will return all rows in table as object
     }
 
-    delete(){
+    async delete(noteId){
         //delete exist note
+        let accessDB = await this.accessStore('readwrite');
+        accessDB.delete(noteId);
     }
 
-    clear(){
+    async clear(){
         // clear all notes on webpage
+        let accessDB = await this.accessStore('readwrite');
+        accessDB.clear()
     }
 
 }
